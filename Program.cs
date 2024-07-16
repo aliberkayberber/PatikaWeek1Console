22785c733d80dd33845e63f04ce9adc8939e48af
aliberkayberber <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>
2024-07-16 18:44:12 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ali Berkay BERBER");
+Console.WriteLine("Patika'nin egitim tecrubeleri ve networku sayesinde hizli bir sekilde dili ögrenecegim ve is firsatlari bulabilecegim icin bu egitimdeyim.");
