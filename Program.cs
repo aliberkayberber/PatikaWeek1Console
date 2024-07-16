51d631712aacbe7aef0577cfecba4e6289ab4bc9
aliberkayberber <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>
2024-07-16 18:49:19 +0300

    add merhaba

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Merhaba");
 Console.WriteLine("Ali Berkay BERBER");
 Console.WriteLine("Patika'nin egitim tecrubeleri ve networku sayesinde hizli bir sekilde dili ögrenecegim ve is firsatlari bulabilecegim icin bu egitimdeyim.");
